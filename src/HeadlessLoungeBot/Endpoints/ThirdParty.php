9c025fc174571c15550f65d569dc6a98305aefba
Soatok Dreamseeker <soatok.dhole@gmail.com>
2019-08-29 04:17:28 -0400

    Ensure

--- src/HeadlessLoungeBot/Endpoints/ThirdParty.php
+++ src/HeadlessLoungeBot/Endpoints/ThirdParty.php
@@ -90,7 +90,9 @@ class ThirdParty extends Endpoint
             'scope' => implode(' ', [
                 'channel:read:subscriptions',
                 'channel_check_subscription',
+                'channel:subscriptions',
                 'channel_subscriptions',
+                'user:subscriptions',
                 'user_subscriptions'
             ]),
             'state' => $cipher
